5dfee7ea13e4ea34b09bcb7d20451b0e0d6a0582
offKevyn <kevynsilva42@gmail.com>
2022-03-05 19:27:09 -0300

    update design

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var player_atual, casas_pl_1 = [], casas_pl_2 = [],
         [1,4,7],
         [2,5,8],
         [3,6,9],
-        [1,5,7],
+        [1,5,9],
         [3,5,7]
     ]
 ;
@@ -23,31 +23,34 @@ function clique(evt)
 
     casa_clicada = parseInt(evt.target.id.replace(/[^0-9]/g,''));
 
+    fonte = 'fonte-' + Math.floor((Math.random() * 8) + 1);
+
     console.log(casa_clicada);
-
-    if(player_atual == pl.player_2)
+    
+    if(casas_pl_2.includes(casa_clicada) == false && casas_pl_1.includes(casa_clicada) == false)
     {
-        player_atual = pl.player_1;
-        element.style.backgroundColor = "green";
-        casas_pl_2.push(casa_clicada);
-       if( ganhou(casas_pl_2))
-       {
-           console.log("Player 2 ganhou");
-       }
+        if(player_atual == pl.player_2) //Player 2
+        {
+            player_atual = pl.player_1;
+            casas_pl_2.push(casa_clicada);
+            element.innerHTML = '<span class="casa-text ' + fonte + '">O</span>';
+            if( ganhou(casas_pl_2))
+            {
+                console.log("Player 2 ganhou");
+            }
+        }
+        else //Player 1
+        {
+            player_atual = pl.player_2;
+            element.innerHTML = '<span class="casa-text ' + fonte + '">X</span>';
+            casas_pl_1.push(casa_clicada);
+            if( ganhou(casas_pl_1))
+            {
+                console.log("Player 1 ganhou");
+            }
+
+        }
     }
-    else
-    {
-        player_atual = pl.player_2;
-        element.style.backgroundColor = "red";
-        casas_pl_1.push(casa_clicada);
-        if( ganhou(casas_pl_1))
-       {
-           console.log("Player 1 ganhou");
-       }
-
-    }
-
-
 }
 
 function ganhou(casas_verificar)
